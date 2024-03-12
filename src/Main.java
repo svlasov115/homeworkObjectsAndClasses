930b2fa6d6d00b19f02c2d878650929902082c9e
Семён <svlasov115@vk.com>
2024-03-12 13:41:01 +0500

    Added tasks solving

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author rowling = new Author("Джоан", "Роулинг");
+        Author dostoevsky = new Author("Фёдор", "Достоевский");
+
+        Book harryPotterAndTheGobletOfFire = new Book("Гарри Поттер и Кубок огня", rowling, 2003);
+        Book crimeAndPunishment = new Book("Преступление и наказание", dostoevsky, 1866);
+
+        print(harryPotterAndTheGobletOfFire);
+        print(crimeAndPunishment);
+
+        crimeAndPunishment.setPublishingYear(2024);
+        print(crimeAndPunishment);
     }
+
+    private static void print(Book book) {
+        System.out.println(
+                "Автор: " + book.getAuthor().getName() + " " + book.getAuthor().getSurname() +
+                        ", название: " + book.getTitle() +", год публикации: " + book.getPublishingYear()
+        );
+    }
+
 }
